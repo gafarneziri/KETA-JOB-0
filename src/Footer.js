97f2b870e42cf8d27a4d72aa18410a088cf8ba69
AndiNezirii <132136065+AndiNezirii@users.noreply.github.com>
2024-12-26 13:46:37 +0100

    finished the responsive uberuns

--- src/Footer.js
+++ src/Footer.js
@@ -7,54 +7,61 @@ import Logo from "./Assets/ketajoblogo.png";
 function Footer() {
   return (
     <div className='bg-[#003049]'>
-    <footer className="bg-white pt-4 pb-4 rounded-t-3xl">
-      {/* Wrapper with blue background to showcase the rounded corners */}
-      <div className="container mx-auto px-6 lg:px-20 ">
-        <div className="flex flex-col md:flex-row justify-between items-start md:items-center -mb-4 space-y-8 md:space-y-0">
-          
-          {/* Left Section - Logo */}
-          <div className="flex-shrink-0">
-            <img src={Logo} alt="Keta Job Logo" className="h-[90px] object-contain" />
+      <footer className="bg-white pt-4 pb-4 rounded-t-3xl">
+        {/* Wrapper with blue background to showcase the rounded corners */}
+        <div className="container mx-auto px-6 lg:px-20">
+          <div className="flex flex-col md:flex-row justify-between items-start md:items-center -mb-4 space-y-8 md:space-y-0">
+            
+            {/* Left Section - Logo */}
+            <div className="flex-shrink-0">
+              <img src={Logo} alt="Keta Job Logo" className="h-[90px] object-contain" />
+            </div>
+
+            {/* Center Section - Social Links */}
+            <div className="flex space-x-8 justify-center items-center">
+              <a href="https://www.linkedin.com/company/keta-real-estate/posts/?feedView=all">
+                <img src={LinkedInIcon} alt="LinkedIn" className="h-10 w-10 hover:opacity-75 transition" />
+              </a>
+              <a href="https://www.facebook.com/ketarealestate1?_rdr">
+                <img src={FacebookIcon} alt="Facebook" className="h-10 w-10 hover:opacity-75 transition" />
+              </a>
+              <a href="https://www.instagram.com/ketarealestate/">
+                <img src={InstagramIcon} alt="Instagram" className="h-10 w-10 hover:opacity-75 transition" />
+              </a>
+            </div>
+
+            {/* Right Section - Contact Information */}
+            <div className="text-sm text-center md:text-left space-y-1">
+              <p className="font-semibold">MOBIL :</p>
+              <p>+41 41 252 18 18</p>
+              <p>+41 41 252 18 19</p>
+              <p className="font-semibold mt-2">EMAIL :</p>
+              <p className="underline">INFO@KETAJOB.CH</p>
+            </div>
           </div>
 
-          {/* Center Section - Social Links */}
-          <div className="flex space-x-8 justify-center items-center">
-            <a href="https://www.linkedin.com/company/keta-real-estate/posts/?feedView=all">
-              <img src={LinkedInIcon} alt="LinkedIn" className="h-10 w-10 hover:opacity-75 transition" />
-            </a>
-            <a href="https://www.facebook.com/ketarealestate1?_rdr">
-              <img src={FacebookIcon} alt="Facebook" className="h-10 w-10 hover:opacity-75 transition" />
-            </a>
-            <a href="https://www.instagram.com/ketarealestate/">
-              <img src={InstagramIcon} alt="Instagram" className="h-10 w-10 hover:opacity-75 transition" />
-            </a>
-          </div>
-
-          {/* Right Section - Contact Information */}
-          <div className="text-sm text-center md:text-left space-y-1">
-            <p className="font-semibold">MOBIL :</p>
-            <p>+41 41 252 18 18</p>
-            <p>+41 41 252 18 19</p>
-            <p className="font-semibold mt-2">EMAIL :</p>
-            <p className="underline">INFO@KETAJOB.CH</p>
+          {/* Address and Privacy Statement */}
+          <div className="pt-8 text-center text-xs text-gray-600 -mb-4">
+            <p>
+              *Personenbezogene Daten (nachfolgend zumeist nur „Daten“ genannt)
+              werden von uns nur im Rahmen der Erforderlichkeit sowie zum Zwecke der Bereitstellung
+              eines funktionsfähigen und <br /> nutzerfreundlichen Internetauftritts, inklusive seiner Inhalte und der dort angebotenen Leistungen, verarbeitet.
+            </p>
           </div>
         </div>
 
-        {/* Address and Privacy Statement */}
-        <div className="  pt-8 text-center text-xs text-gray-600 -mb-4">
-          <p>*Personenbezogene Daten (nachfolgend zumeist nur „Daten“ genannt) werden von uns nur im Rahmen der Erforderlichkeit sowie zum Zwecke der Bereitstellung
-             eines funktionsfähigen und <br/> nutzerfreundlichen Internetauftritts, inklusive seiner Inhalte und der dort angebotenen Leistungen, verarbeitet.</p>
-        </div>
-        <div className='border border-gray-400 mt-8 -ml-20 -mr-20'></div>
+        {/* Full-Width Line */}
+        <div className="w-full border-t border-gray-400 mt-8"></div>
 
-        {/* Footer Bottom */}
-        <div className="mt-4 flex justify-between text-xs  text-gray-600">
-          <p>© 2024 KETA GROUP, ALL RIGHTS RESERVED</p>
-          <p>OTHMARSTRASSE 8, 8008 ZÜRICH, SCHWEIZ</p>
-          <p>BIRKENSTRASSE 47, 6343 ROTKREUZ, SCHWEIZ</p>
+        <div className="container mx-auto px-6 lg:px-20">
+          {/* Footer Bottom */}
+          <div className="mt-4 flex justify-between text-xs text-gray-600">
+            <p>© 2024 KETA GROUP, ALL RIGHTS RESERVED</p>
+            <p>OTHMARSTRASSE 8, 8008 ZÜRICH, SCHWEIZ</p>
+            <p>BIRKENSTRASSE 47, 6343 ROTKREUZ, SCHWEIZ</p>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
     </div>
   );
 }
--- src/components/UberUNSN.js
+++ src/components/UberUNSN.js
@@ -7,119 +7,109 @@ import emailIcon from '../Assets/emailIcon.png';
 import phoneIcon from '../Assets/phoneIcon.png';
 import languageIcon from '../Assets/languageIcon.png';
 
-
 const Uberuns = () => {
   return (
-<div className="Uberuns bg-[#003049] text-white py-10 font-Montserrat relative">
-  {/* Full-width Image */}
-  <div className="relative w-full">
-    <img src={Uberuns1} alt="uberuns" className="w-full h-auto" />
+    <div className="Uberuns bg-[#003049] text-white py-6 sm:py-10 font-Montserrat relative">
+      {/* Full-width Image */}
+      <div className="relative w-full">
+        <img
+          src={Uberuns1}
+          alt="uberuns"
+          className="w-full h-[200px] sm:h-auto object-cover"
+        />
 
-    {/* Centered Title on Image */}
-    <div className="absolute inset-0 flex items-center justify-center">
-      <div className="flex items-center justify-center w-full">
-        <hr className="border-t-2 border-white w-2/6" />
-        <p className="  font-Montserrat  text-[70px] mx-4">ÜBER UNS</p>
-        <hr className="border-t-2 border-white w-2/6" />
+        {/* Centered Title on Image */}
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="flex items-center justify-center w-full">
+            <hr className="border-t-2 border-white w-2/6" />
+            <p className="font-Montserrat text-[22px] sm:text-[70px] mx-2 sm:mx-4 whitespace-nowrap">ÜBER UNS</p>
+            <hr className="border-t-2 border-white w-2/6" />
+          </div>
+        </div>
+      </div>
+
+      {/* Content Section */}
+      <div className="relative z-10 flex flex-col items-center text-center py-8 sm:py-16 px-2 sm:px-4">
+        <p className="text-white text-left text-[10px] sm:text-lg mt-4 sm:mt-8 sm:text-[28px] leading-relaxed">
+          So denken und handeln wir: Menschen sehen wir nie nur als Kostenfaktoren. Vielmehr sind es die richtigen Mitarbeiter, die für einen gesunden Betrieb sorgen.
+          <br /><br />
+          Unsere Dienstleistungen beginnen wir wie Ärzte mit einer Anamnese: Wir möchten wissen, wo der Schuh drückt.
+          <br /><br />
+          Daraufhin suchen wir schnell und unkompliziert eine Lösung für Ihr Problem. Wir ermöglichen kurzfristige Einsätze von Fachkräften.
+          <br /><br />
+          Obwohl wir professionell vorgehen und uns mit Zahlen und Fakten auskennen, sind wir überzeugt, dass man letztlich nur mit dem Herzen gut sieht. Darum investieren wir viel Zeit darin, Menschen wahrzunehmen, sie zu unterstützen und den richtigen Job für sie zu finden.
+          <br /><br />
+          Wir leisten gerne einen Sondereinsatz, damit Ihr Betrieb auf Hochtouren laufen kann. Wir legen einen großen Wert auf faire Löhne und Arbeitsbedingungen sowie ein vertrauensvolles Miteinander. Das zählt sich langfristig für alle aus.
+        </p>
+      </div>
+
+      {/* Profiles Section */}
+      <div className="flex flex-col sm:flex-row justify-center items-center sm:space-x-8 py-8 sm:py-10 space-y-8 sm:space-y-0 px-2 sm:px-4">
+        {/* Profile 1 */}
+        <div className="text-white p-4 sm:p-6">
+          <img src={nidaImage} alt="Nida Hamiti" className="w-full sm:w-96 h-auto object-cover rounded-lg mx-auto mb-4" />
+          <h2 className="text-center text-xl">Frau</h2>
+          <h3 className="text-center text-2xl font-semibold">Nida Hamiti</h3>
+          <p className="text-center text-md mb-2">CEO</p>
+          <hr className="border-t border-gray-400 my-4 mx-8 sm:mx-14" />
+          <div className="flex items-center justify-center space-x-2">
+            <img src={languageIcon} alt="Language Icon" className="w-5 h-5" />
+            <p className="text-sm">Deutsch, Englisch, Albanisch</p>
+          </div>
+          <div className="flex items-center justify-center space-x-2 mt-4">
+            <img src={emailIcon} alt="Email Icon" className="w-5 h-5" />
+            <a href="mailto:nida@keta-group.ch" className="text-sm">nh@keta-group.ch</a>
+          </div>
+          <div className="flex items-center justify-center space-x-2 mt-4">
+            <img src={phoneIcon} alt="Phone Icon" className="w-5 h-5" />
+            <a href="tel:+41412521818" className="text-sm">+41 41 252 18 18</a>
+          </div>
+        </div>
+
+        {/* Profile 2 */}
+        <div className="text-white p-4 sm:p-6">
+          <img src={nathalieImage} alt="Nathalie Dellspreger" className="w-full sm:w-96 h-auto object-cover rounded-lg mx-auto mb-4" />
+          <h2 className="text-center text-xl">Frau</h2>
+          <h3 className="text-center text-2xl font-semibold">Nathalie Dellspreger</h3>
+          <p className="text-center text-md mb-2">Stv. Geschäftsführerin</p>
+          <hr className="border-t border-gray-400 my-4 mx-8 sm:mx-14" />
+          <div className="flex items-center justify-center space-x-2">
+            <img src={languageIcon} alt="Language Icon" className="w-5 h-5" />
+            <p className="text-sm">Deutsch, Englisch, Portugiesisch</p>
+          </div>
+          <div className="flex items-center justify-center space-x-2 mt-4">
+            <img src={emailIcon} alt="Email Icon" className="w-5 h-5" />
+            <a href="mailto:nathalie@keta-group.ch" className="text-sm">nd@keta-group.ch</a>
+          </div>
+          <div className="flex items-center justify-center space-x-2 mt-4">
+            <img src={phoneIcon} alt="Phone Icon" className="w-5 h-5" />
+            <a href="tel:+41412521818" className="text-sm">+41 41 252 18 18</a>
+          </div>
+        </div>
+
+        {/* Profile 3 */}
+        <div className="text-white p-4 sm:p-6">
+          <img src={fatimeImage} alt="Fatime Hamiti" className="w-full sm:w-96 h-auto object-cover rounded-lg mx-auto mb-4" />
+          <h2 className="text-center text-xl">Frau</h2>
+          <h3 className="text-center text-2xl font-semibold">Fatime Hamiti</h3>
+          <p className="text-center text-md mb-2">CFO</p>
+          <hr className="border-t border-gray-400 my-4 mx-8 sm:mx-14" />
+          <div className="flex items-center justify-center space-x-2">
+            <img src={languageIcon} alt="Language Icon" className="w-5 h-5" />
+            <p className="text-sm">Deutsch, Englisch, Albanisch</p>
+          </div>
+          <div className="flex items-center justify-center space-x-2 mt-4">
+            <img src={emailIcon} alt="Email Icon" className="w-5 h-5" />
+            <a href="mailto:fatime@keta-group.ch" className="text-sm">fh@keta-group.ch</a>
+          </div>
+          <div className="flex items-center justify-center space-x-2 mt-4">
+            <img src={phoneIcon} alt="Phone Icon" className="w-5 h-5" />
+            <a href="tel:+41412521818" className="text-sm">+41 41 252 18 18</a>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  );
+};
 
-  {/* Content Section */}
-  <div className="relative z-10 flex flex-col items-center text-center py-16 px-4">
-    {/* Text Content */}
-    <p className="text-white text-left text-lg mt-8 text-[28px] ml-4 mr-4 leading-relaxed">
-      So denken und handeln wir: Menschen sehen wir nie nur als Kostenfaktoren. Vielmehr sind es die richtigen Mitarbeiter, die für einen gesunden Betrieb sorgen.
-      <br /><br />
-      Unsere Dienstleistungen beginnen wir wie Ärzte mit einer Anamnese: Wir möchten wissen, wo der Schuh drückt.
-      <br /><br />
-      Daraufhin suchen wir schnell und unkompliziert eine Lösung für Ihr Problem. Wir ermöglichen kurzfristige Einsätze von Fachkräften.
-      <br /><br />
-      Obwohl wir professionell vorgehen und uns mit Zahlen und Fakten auskennen, sind wir überzeugt, dass man letztlich nur mit dem Herzen gut sieht. Darum investieren wir viel Zeit darin, Menschen wahrzunehmen, sie zu unterstützen und den richtigen Job für sie zu finden.
-      <br /><br />
-      Wir leisten gerne einen Sondereinsatz, damit Ihr Betrieb auf Hochtouren laufen kann. Wir legen einen großen Wert auf faire Löhne und Arbeitsbedingungen sowie ein vertrauensvolles Miteinander. Das zählt sich langfristig für alle aus.
-    </p>
-  </div>
-
-
-
-
-  <div className="flex justify-center space-x-8 py-10">
-  {/* Profile 1 */}
-  <div className="text-white p-6">
-    <img src={nidaImage} alt="Nida Hamiti" className="w-96 h-auto object-cover rounded-lg mx-auto mb-4" />
-    <h2 className="text-center text-xl ">Frau</h2>
-    <h3 className="text-center text-2xl font-semibold">Nida Hamiti</h3>
-    <p className="text-center text-md mb-2">CEO</p>
-    <hr className="border-t border-gray-400 my-4 mx-14" />
-    <div className="flex items-center justify-center space-x-2">
-      <img src={languageIcon} alt="Language Icon" className="w-5 h-5" />
-      <p className="text-sm">Deutsch, Englisch, Albanisch</p>
-    </div>
-    <div className="flex items-center justify-center space-x-2 mt-4">
-      <img src={emailIcon} alt="Email Icon" className="w-5 h-5" />
-      <a href="mailto:nida@keta-group.ch" className="text-sm">nh@keta-group.ch</a>
-    </div>
-    <div className="flex items-center justify-center space-x-2 mt-4">
-      <img src={phoneIcon} alt="Phone Icon" className="w-5 h-5" />
-      <a href="tel:+41412521818" className="text-sm">+41 41 252 18 18</a>
-    </div>
-  </div>
-
-  {/* Profile 2 */}
-  <div className="text-white p-6">
-    <img src={nathalieImage} alt="Nathalie Dellspreger" className="w-96 h-auto object-cover rounded-lg mx-auto mb-4" />
-    <h2 className="text-center text-xl ">Frau</h2>
-    <h3 className="text-center text-2xl font-semibold">Nathalie Dellspreger</h3>
-    <p className="text-center text-md mb-2">Stv. Geschäftsführerin</p>
-    <hr className="border-t border-gray-400 my-4 mx-14" />
-    <div className="flex items-center justify-center space-x-2">
-      <img src={languageIcon} alt="Language Icon" className="w-5 h-5" />
-      <p className="text-sm">Deutsch, Englisch, Portugiesisch</p>
-    </div>
-    <div className="flex items-center justify-center space-x-2 mt-4">
-      <img src={emailIcon} alt="Email Icon" className="w-5 h-5" />
-      <a href="mailto:nathalie@keta-group.ch" className="text-sm">nd@keta-group.ch</a>
-    </div>
-    <div className="flex items-center justify-center space-x-2 mt-4">
-      <img src={phoneIcon} alt="Phone Icon" className="w-5 h-5" />
-      <a href="tel:+41412521818" className="text-sm">+41 41 252 18 18</a>
-    </div>
-  </div>
-
-  {/* Profile 3 */}
-  <div className="text-white p-6">
-    <img src={fatimeImage} alt="Fatime Hamiti" className="w-96 h-auto object-cover rounded-lg mx-auto mb-4" />
-    <h2 className="text-center text-xl ">Frau</h2>
-    <h3 className="text-center text-2xl font-semibold">Fatime Hamiti</h3>
-    <p className="text-center text-md mb-2">CFO</p>
-    <hr className="border-t border-gray-400 my-4 mx-14" />
-    <div className="flex items-center justify-center space-x-2">
-      <img src={languageIcon} alt="Language Icon" className="w-5 h-5" />
-      <p className="text-sm">Deutsch, Englisch, Albanisch</p>
-    </div>
-    <div className="flex items-center justify-center space-x-2 mt-4">
-      <img src={emailIcon} alt="Email Icon" className="w-5 h-5" />
-      <a href="mailto:fatime@keta-group.ch" className="text-sm">fh@keta-group.ch</a>
-    </div>
-    <div className="flex items-center justify-center space-x-2 mt-4">
-      <img src={phoneIcon} alt="Phone Icon" className="w-5 h-5" />
-      <a href="tel:+41412521818" className="text-sm">+41 41 252 18 18</a>
-    </div>
-  </div>
-</div>
-
-
-
-
-</div>
-
-
-
-  
-
-
-  )
-}
-
-export default Uberuns ;
+export default Uberuns;
